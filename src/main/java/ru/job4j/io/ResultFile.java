e34921bfb22891e0e18ca3b0b529fc8eb743434b
wertifag <gringrowin@gmail.com>
2022-04-27 10:18:26 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,31 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static int[][] multiple(int size) {
+        int[][] matrix = new int[size][size];
+        for (int row = 0; row < size; row++) {
+            for (int cell = 0; cell < size; cell++) {
+                matrix[row][cell] = (row + 1) * (cell + 1);
+
+            }
+        }
+        return matrix;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = multiple(9);
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for (int[] y : matrix) {
+                out.write(System.lineSeparator().getBytes());
+                for (int x : y) {
+                    out.write(Integer.toString(x).getBytes());
+                    out.write(" ".getBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
