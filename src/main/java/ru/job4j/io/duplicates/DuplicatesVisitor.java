01b925b1587ad684c515574fb56683a863b8f72f
wertifag <gringrowin@gmail.com>
2022-05-15 22:53:40 +0300

    4.2. Поиск дубликатов [#315066]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -5,10 +5,7 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
@@ -17,10 +14,8 @@ public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         FileProperty fileProperty = new FileProperty(file.toFile().length(), file.getFileName().toString());
-        if (allFilesMap.containsKey(fileProperty)) {
-           allFilesMap.get(fileProperty).add(file);
-        } else {
-            allFilesMap.put(fileProperty, new ArrayList<>(List.of(file)));
+        if (allFilesMap.putIfAbsent(fileProperty, new ArrayList<>(List.of(file))) != null) {
+            allFilesMap.get(fileProperty).add(file);
         }
         return super.visitFile(file, attrs);
     }
