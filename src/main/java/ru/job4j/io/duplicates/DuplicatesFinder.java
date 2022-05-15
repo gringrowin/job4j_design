2c77e9ed0572769f38e47cd79ccc7dd3114f4a7f
wertifag <gringrowin@gmail.com>
2022-05-15 15:44:49 +0300

    4.2. Поиск дубликатов [#315066]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -3,6 +3,7 @@ package ru.job4j.io.duplicates;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.Map;
 
 public class DuplicatesFinder {
@@ -10,13 +11,13 @@ public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
         DuplicatesVisitor duplicatesVisitor = new DuplicatesVisitor();
         Files.walkFileTree(Path.of("./"), duplicatesVisitor);
-        printDuplicates(duplicatesVisitor.getDuplicates());
+        printDuplicates(duplicatesVisitor.getAllFilesMap());
     }
 
-    public static void printDuplicates(Map<Path, FileProperty> duplicates) {
-        for (Path file : duplicates.keySet()) {
-            System.out.println(file.toAbsolutePath());
-            System.out.println(duplicates.get(file));
-        }
+    public static void printDuplicates(Map<FileProperty, List<Path>> files) {
+        files.values()
+                .stream()
+                .filter(value -> value.size() > 1)
+                .forEach(value -> value.forEach(System.out::println));
     }
 }
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -5,27 +5,27 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
-    final Map<FileProperty, Path> allFilesMap = new HashMap<>();
-    final Map<Path, FileProperty> duplicates = new HashMap<>();
+    final Map<FileProperty, List<Path>> allFilesMap = new HashMap<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         FileProperty fileProperty = new FileProperty(file.toFile().length(), file.getFileName().toString());
         if (allFilesMap.containsKey(fileProperty)) {
-            duplicates.putIfAbsent(allFilesMap.get(fileProperty), fileProperty);
-            duplicates.putIfAbsent(file, fileProperty);
+           allFilesMap.get(fileProperty).add(file);
         } else {
-            allFilesMap.put(fileProperty, file);
+            allFilesMap.put(fileProperty, new ArrayList<>(List.of(file)));
         }
         return super.visitFile(file, attrs);
     }
 
-    public Map<Path, FileProperty> getDuplicates() {
-        return duplicates;
+    public Map<FileProperty, List<Path>> getAllFilesMap() {
+        return allFilesMap;
     }
 }
