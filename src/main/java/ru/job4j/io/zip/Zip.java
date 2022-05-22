947d1bf7531ab876102bd767ae815dec0d6c396b
wertifag <gringrowin@gmail.com>
2022-05-22 21:56:48 +0300

    5. Валидация параметров запуска. [#246865 #301616]

--- src/main/java/ru/job4j/io/zip/Zip.java
+++ src/main/java/ru/job4j/io/zip/Zip.java
@@ -1,6 +1,7 @@
 package ru.job4j.io.zip;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -23,25 +24,31 @@ public class Zip {
         }
     }
 
-    public void packSingleFile(Path source, Path target) {
-        try (ZipOutputStream zip
-                     = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target.toString())))) {
-            zip.putNextEntry(new ZipEntry(source.toString()));
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source.toString()))) {
-                zip.write(out.readAllBytes());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+    private static boolean argsValid(String[] args) {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Need three arguments");
         }
+        if (!Files.isDirectory(Path.of(args[0]))) {
+            throw new IllegalArgumentException("Incorrect path");
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException("Incorrect source file extension");
+        }
+        if (!args[2].endsWith(".zip")) {
+            throw new IllegalArgumentException("Incorrect target file extension");
+        }
+        return true;
     }
 
     public static void main(String[] args) {
-        ArgsName name = ArgsName.of(args);
-        Zip zip = new Zip();
-        List<Path> sources = Search.search(Paths.get(name.get("d")), p -> !p.toFile()
-                        .getName()
-                        .endsWith(name.get("e")));
-        Path target = Paths.get(name.get("o"));
-        zip.packFiles(sources, target);
+        if (argsValid(args)) {
+            ArgsName name = ArgsName.of(args);
+            Zip zip = new Zip();
+            List<Path> sources = Search.search(Paths.get(name.get("d")), p -> !p.toFile()
+                    .getName()
+                    .endsWith(name.get("e")));
+            Path target = Paths.get(name.get("o"));
+            zip.packFiles(sources, target);
+        }
     }
 }
