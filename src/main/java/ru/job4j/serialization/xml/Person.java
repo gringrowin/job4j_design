1f2b96fbed4f941282085ae555e7c09a63482c00
wertifag <gringrowin@gmail.com>
2023-11-26 09:16:34 +0300

    4. JAXB. Преобразование XML в POJO. [#315063]- учебная часть

---
+++ src/main/java/ru/job4j/serialization/xml/Person.java
@@ -0,0 +1,41 @@
+package ru.job4j.serialization.xml;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Arrays;
+
+@XmlRootElement(name = "person")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Person {
+
+    @XmlAttribute
+    private boolean sex;
+
+    @XmlAttribute
+    private int age;
+
+    private Contact contact;
+
+    private String[] statuses;
+
+    public Person() { }
+
+    public Person(boolean sex, int age, Contact contact, String... statuses) {
+        this.sex = sex;
+        this.age = age;
+        this.contact = contact;
+        this.statuses = statuses;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{"
+                + "sex=" + sex
+                + ", age=" + age
+                + ", contact=" + contact
+                + ", statuses=" + Arrays.toString(statuses)
+                + '}';
+    }
+}
